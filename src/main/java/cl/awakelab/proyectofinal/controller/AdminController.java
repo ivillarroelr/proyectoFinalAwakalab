372ae0eca23611ed5d3c00ac23a05ecf0d708040
katramlar <60943485+katramlar@users.noreply.github.com>
2020-07-31 12:31:05 -0400

    se crea CRUD de clientes desde vista administrador

--- src/main/java/cl/awakelab/proyectofinal/controller/AdminController.java
+++ src/main/java/cl/awakelab/proyectofinal/controller/AdminController.java
@@ -2,8 +2,10 @@ package cl.awakelab.proyectofinal.controller;
 
 import cl.awakelab.proyectofinal.dto.ProfesionalDTO;
 import cl.awakelab.proyectofinal.dto.UsersDTO;
+import cl.awakelab.proyectofinal.model.Cliente;
 import cl.awakelab.proyectofinal.model.Profesional;
 import cl.awakelab.proyectofinal.model.Users;
+import cl.awakelab.proyectofinal.service.IClienteService;
 import cl.awakelab.proyectofinal.service.IProfesionalService;
 import cl.awakelab.proyectofinal.service.IUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,9 @@ public class AdminController {
     public String admin() {
         return "indexadministrador";
     }
+
+
+    //CRUD DE PROFESIONALES
 
     @GetMapping({"/verprofesional"})
     public ModelAndView verprofesional() {
@@ -77,10 +82,106 @@ public class AdminController {
         return model;
     }
 
-    @GetMapping({"/vercliente"})
-    public String vercliente() {
-        return "vercliente";
+    @GetMapping("/editar/{username}")
+    public String editarProfesional(@PathVariable("username") String username, Model model){
+        Users usuario = serviceUsuario.leerPorUsername(username);
+        model.addAttribute("usuario", usuario);
+        return "editarprofesional";
     }
+
+    @PostMapping("/editarprofesional/{username}")
+    public String updateProfesional(@PathVariable("username") String username, @Valid Users usuario,
+                                    BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            usuario.setUsername(username);
+            return "redirect:/verprofesional";
+        }
+        serviceUsuario.modificar(usuario);
+        model.addAttribute("profesionales", serviceProfesional.listar());
+        return "redirect:/verprofesional";
+    }
+
+    @DeleteMapping("/borrarprofesional/{username}")
+    public String eliminarProfesional(@PathVariable("username") String username){
+        serviceUsuario.eliminar(username);
+        return "redirect:/verprofesional";
+    }
+
+    //CRUD DE CLIENTES
+
+    @Autowired
+    private IClienteService serviceCliente;
+
+    @GetMapping({"/vercliente"})
+    public ModelAndView vercliente() {
+        ModelAndView model = new ModelAndView();
+        List<Cliente> clientes = new ArrayList<Cliente>();
+        clientes = serviceCliente.listar();
+        model.addObject("clientes", clientes);
+        model.setViewName("vercliente");
+        return model;
+    }
+
+    @GetMapping("/nuevocliente")
+    public ModelAndView nuevocliente(){
+        ModelAndView model = new ModelAndView();
+        List<Users> clientes = new ArrayList<Users>();
+        clientes = serviceUsuario.listar();
+        model.addObject("clientes", clientes);
+        model.setViewName("nuevocliente");
+        return model;
+    }
+
+    @PostMapping("/crearcliente")
+    public ModelAndView crearCliente(@ModelAttribute("cliente") UsersDTO cliente){
+        ModelAndView model = new ModelAndView();
+        Users pr = new Users();
+        Cliente cl = new Cliente();
+        pr.setUsername(cliente.getUsername());
+        pr.setApellido(cliente.getApellido());
+        pr.setEnabled(cliente.getEnabled());
+        pr.setNombre(cliente.getNombre());
+        pr.setPassword(cliente.getPassword());
+        pr.setRut(cliente.getRut());
+        pr.setTipoUsuario(cliente.getTipoUsuario());
+        serviceUsuario.registrar(pr);
+        cl.setUsername(cliente.getUsername());
+        cl.setUsuario(pr);
+        serviceCliente.registrar(cl);
+        List<Cliente> clientes = new ArrayList<Cliente>();
+        clientes = serviceCliente.listar();
+        model.addObject("clientes", clientes);
+        model.setViewName("vercliente");
+        return model;
+    }
+
+
+    @GetMapping("/editarcl/{username}")
+    public String editarCliente(@PathVariable("username") String username, Model model){
+        Users usuario = serviceUsuario.leerPorUsername(username);
+        model.addAttribute("usuario", usuario);
+        return "editarcliente";
+    }
+
+    @PostMapping("/editarcliente/{username}")
+    public String updateCliente(@PathVariable("username") String username, @Valid Users usuario,
+                                    BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            usuario.setUsername(username);
+            return "redirect:/vercliente";
+        }
+        serviceUsuario.modificar(usuario);
+        model.addAttribute("clientes", serviceCliente.listar());
+        return "redirect:/vercliente";
+    }
+
+    @DeleteMapping("/borrarcliente/{username}")
+    public String eliminarcliente(@PathVariable("username") String username){
+        serviceUsuario.eliminar(username);
+        return "redirect:/vercliente";
+    }
+
+
 
     @GetMapping({"/pagos"})
     public String verpagos() {
@@ -97,29 +198,6 @@ public class AdminController {
         return "actividad";
     }
 
-    @GetMapping("/editar/{username}")
-    public String editarProfesional(@PathVariable("username") String username, Model model){
-        Users usuario = serviceUsuario.leerPorUsername(username);
-        model.addAttribute("usuario", usuario);
-        return "editarprofesional";
-    }
 
-    @PostMapping("/editarprofesional/{username}")
-    public String updateProfesional(@PathVariable("username") String username, @Valid Users usuario,
-                             BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            usuario.setUsername(username);
-            return "redirect:/verprofesional";
-        }
-        serviceUsuario.modificar(usuario);
-        model.addAttribute("profesionales", serviceProfesional.listar());
-        return "redirect:/verprofesional";
-    }
-
-    @DeleteMapping("/borrarprofesional/{username}")
-    public String eliminarProfesional(@PathVariable("username") String username){
-        serviceUsuario.eliminar(username);
-        return "redirect:/verprofesional";
-    }
 
 }
